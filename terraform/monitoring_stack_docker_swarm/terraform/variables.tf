4975d949b9b4706bfa5ac8a9ea10656ccbe452dd
Denis Kurskiy <crazyozzy001@gmail.com>
2022-10-23 18:45:31 +0300

    Add some examples

---
+++ terraform/monitoring_stack_docker_swarm/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1grats6mcasijcqai07"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1giebh9ehdhj9fgbfjp"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8v4titvc2erjm4g8ba"
+}
