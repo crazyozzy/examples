74fbd4845453122588bcf6c3d24d98400aa58459
Denis Kurskiy <crazyozzy001@gmail.com>
2022-11-30 23:32:49 +0300

    Stepic Go course - Functions

--- Go/Stepic/temp.go
+++ Go/Stepic/temp.go
@@ -1,45 +1,22 @@
 package main
 
-type Rider struct {
-	On bool
-	Ammo int
-	Power int
-}
-
-func (r *Rider) Shoot () bool {
-	if r.On == false {
-		return false
-	}
-
-	if r.Ammo > 0 {
-		r.Ammo--
-		return true
-	} else {
-		return false
-	}
-}
-
-func (r *Rider) RideBike () bool {
-	if r.On == false {
-		return false
-	}
-
-	if r.Power > 0 {
-		r.Power--
-		return true
-	} else {
-		return false
-	}
-}
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+	var iStrText string
+	
+	fmt.Scan(&iStrText)
 
-	testStruct := &Rider{true, 5, 3}
-	/*
-	 * Экземпляр созданной вами структуры необходимо передать в качестве
-	 * аргумента функции testStruct, которая выполнит проверку соблюдения
-	 * всех условий задания/
-	 */
+	for _, v := range iStrText {
+		fmt.Println(iStrText, string(v))
+		if strings.Count(iStrText, string(v)) > 1 {
+			iStrText = strings.Trim(iStrText, string(v))
+			fmt.Println(iStrText, string(v))
+		}
+	}
 
-// }
+	fmt.Println(iStrText)
 }
