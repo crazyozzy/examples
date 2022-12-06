629c52a7af23168c4a1b7915acb97aac57bc2bc8
Denis Kurskiy <crazyozzy001@gmail.com>
2022-12-06 23:31:45 +0300

    Stepic Go course - Converts

--- Go/Stepic/temp.go
+++ Go/Stepic/temp.go
@@ -1,11 +1,33 @@
-func M() float64 {
-	return p * v
-}
+package main
 
-func W() float64 {
-	return math.Sqrt(k / M())
-}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func T() float64 {
-	return 6 / W()
+func main() {
+	var iStr string
+	var iStrSlice []string
+	var iFloatSlice [2]float64
+
+	iStdin := bufio.NewScanner(os.Stdin)
+	if iStdin.Scan() {
+		iStr = iStdin.Text()
+	}
+
+	iStrSlice = strings.Split(iStr, ";")
+
+	iStrSlice[0] = strings.Replace(iStrSlice[0], " ", "", -1)
+	iStrSlice[1] = strings.Replace(iStrSlice[1], " ", "", -1)
+	iStrSlice[0] = strings.Replace(iStrSlice[0], ",", ".", -1)
+	iStrSlice[1] = strings.Replace(iStrSlice[1], ",", ".", -1)
+	fmt.Println(iStrSlice)
+
+	iFloatSlice[0], _ = strconv.ParseFloat(iStrSlice[0], 64)
+	iFloatSlice[1], _ = strconv.ParseFloat(iStrSlice[1], 64)
+
+	fmt.Printf("%.4f", iFloatSlice[0] / iFloatSlice[1])
 }
