83855593febcf6b9fb916c60a00fd2680da368ad
Denis Kurskiy <crazyozzy001@gmail.com>
2022-12-08 23:49:01 +0300

    Stepic Go course - Interfaces

--- Go/Stepic/temp.go
+++ Go/Stepic/temp.go
@@ -1,31 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+	"encoding/json" // пакет используется для проверки ответа, не удаляйте его
+	"fmt"           // пакет используется для проверки ответа, не удаляйте его
+	"os"            // пакет используется для проверки ответа, не удаляйте его
 )
 
+
+
 func main() {
-	fn := func(iN uint) uint {
-		iStr := strconv.Itoa(int(iN))
-		oStr := ""
+	value1, value2, operation := readTask() // исходные данные получаются с помощью этой функции
+                                            // все полученные значения имеют тип пустого интерфейса
+	var iFloat1, iFloat2 float64
 
-		for _, v := range iStr {
-			switch string(v) {
-			case "2", "4", "6", "8":
-				oStr += string(v)
-			}
-		}
-		
-		temp, _ := strconv.ParseUint(oStr, 10, 64)
-		iN = uint(temp)
-
-		if iN == 0 {
-			return 100
-		} else {
-			return iN
-		}
+	switch v := value1.(type) {
+	case float64:
+		iFloat1 = v
+	default:
+		fmt.Printf("value=%v: %T", v, v)
+		os.Exit(1)
 	}
 
-	fmt.Print(fn(727178))
+	switch v := value2.(type) {
+	case float64:
+		iFloat2 = v
+	default:
+		fmt.Printf("value=%v: %T", v, v)
+		os.Exit(1)
+	}
+
+	switch operation {
+	case "+":
+		fmt.Printf("%.4f", iFloat1 + iFloat2)
+	case "-":
+		fmt.Printf("%.4f", iFloat1 - iFloat2)
+	case "*":
+		fmt.Printf("%.4f", iFloat1 * iFloat2)
+	case "/":
+		fmt.Printf("%.4f", iFloat1 / iFloat2)
+	default:
+		fmt.Print("неизвестная операция")
+	}
 }
