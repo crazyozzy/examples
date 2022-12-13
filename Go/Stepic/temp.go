05b5ab46f3ad5830430f0768bc03396fd1eb7b64
Denis Kurskiy <crazyozzy001@gmail.com>
2022-12-13 23:40:45 +0300

    Stepic Go course - Parallelism part 1

--- Go/Stepic/temp.go
+++ Go/Stepic/temp.go
@@ -1,26 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"time"
-)
+import "strings"
 
-func main() {
-	const timeFormat = "02.01.2006 15:04:05"
-	const startTime = 1589570165
-	var min, sec int
+func removeDuplicates(inputStream, outputStream chan string) {
+	dupl := make(map[string]bool)
 
-	fmt.Scanf("%d мин. %d сек.", &min, &sec)
-	iStr := fmt.Sprintf("0h%dm%ds", min, sec)
-
-	iDur, err := time.ParseDuration(iStr)
-	if err != nil && err != io.EOF {
-		panic(err)
+	for v := range inputStream {
+		if _, ok := dupl[v]; !ok {
+			outputStream <- v
+			dupl[v] = true
+		}
 	}
 
-	iTime := time.Unix(startTime, 0).UTC()
-	iTime = iTime.Add(iDur)
-
-	fmt.Println(iTime.Format(time.UnixDate))
+	close(outputStream)
 }
