c1e3b112f498ae39dbea8cbe677cf64b4626cff2
Denis Kurskiy <crazyozzy001@gmail.com>
2022-12-15 00:18:46 +0300

    Stepic Go course - Parallelism part 2

--- Go/Stepic/temp.go
+++ Go/Stepic/temp.go
@@ -1,16 +1,54 @@
 package main
 
-import "strings"
+import (
+	"fmt" // пакет используется для проверки выполнения условия задачи, не удаляйте его
+	"sync"
+	// "time" // пакет используется для проверки выполнения условия задачи, не удаляйте его
+)
 
-func removeDuplicates(inputStream, outputStream chan string) {
-	dupl := make(map[string]bool)
+func merge2Channels(fn func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
+	locker := new(sync.Mutex)
 
-	for v := range inputStream {
-		if _, ok := dupl[v]; !ok {
-			outputStream <- v
-			dupl[v] = true
+	go func(){
+		for i := 0; i < n; i++ {
+			
+			// locker.Lock()
+			// x1 := <- in1
+			// x2 := <- in2
+			// locker.Unlock()
+			// go func (x1, x2, i int)  {
+			// 	out <- fn(x1) + fn(x2)
+			// }(x1, x2, i)
 		}
-	}
+	}()
+}
 
-	close(outputStream)
+const N = 5
+
+func main() {
+
+    in1, in2, out := make(chan int, 1), make(chan int, 1), make(chan int, N)
+
+    merge2Channels(some, in1, in2, out, N)
+
+    for i := 0; i < N; i++ {
+
+        in1 <- i
+
+        in2 <- i
+
+    }
+
+    for i := 0; i < N; i++ {
+
+        fmt.Println(<-out)
+
+    }
+
+}
+
+func some(val int) int {
+
+    return val
+
 }
